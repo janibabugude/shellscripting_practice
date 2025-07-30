b101eda22decd1e52820f3ea6aca409ead052e3d
janibabugude <janibabugude@gmail.com>
2025-07-30 16:27:59 +0530

    update argumentvariable.sh

--- argumentvariable.sh
+++ argumentvariable.sh
@@ -6,7 +6,10 @@ variable_name2=$2
 echo "$variable_name1 and $variable_name2"
 echo "This is your script_file: $0"
 echo "number of argument passed: $#"
+echo " this is last argument: ${n}"
+echo "what are the aruguments passed in between ${5}-${n}"
 echo "what arguments are passed by user is: $@"
 echo "what are the arguments are passed by user is: $*"
+asdf
 echo "provide exit status code for previous command 0 for succuss 127 for failed $?"
 echo "this is process id : $$" 
