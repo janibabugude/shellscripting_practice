3981d591e063c934df555854c56bb1e0d2605f47
janibabugude <janibabugude@gmail.com>
2025-07-30 15:07:08 +0530

    update user_input.sh

--- user_input.sh
+++ user_input.sh
@@ -1,13 +1,19 @@
-# take input from user using read coomand
+# take input from user next line using read coomand
 <<com
 echo "enter the number : "
 read num1
 echo "enter the number: "
 read num2
-com
+
+# take input from user same line using read -p (prompt)coomand
 read -p "enter your first number: " num1
 read -p "enter your second number: " num2
 addition=$(($num1+$num2))
 echo "The addition of two numbers ${num1} and ${num2} is $addition"
 multiplication=$(($num1 * $num2))
 echo -n "the multipilication of two numbers $num1 and $num2 is: $multiplication"
+com
+# take input from user with secert data like hiding password using read -s commond
+read -p "enter username" username
+read -s -p "enter password" password
+echo "the username is $username and password is $password" 
