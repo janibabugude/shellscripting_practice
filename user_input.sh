edd349b9aa20e801fb97811fd1dafb1d5ecb6691
janibabugude <janibabugude@gmail.com>
2025-07-30 14:29:07 +0530

    user_input.sh

--- user_input.sh
+++ user_input.sh
@@ -1,7 +1,11 @@
 # take input from user using read coomand
+<<com
 echo "enter the number : "
 read num1
 echo "enter the number: "
 read num2
+com
+echo "enter your first number: " read -p num1
+echo "enter your second number: " read -p num2
 addition=$(($num1+$num2))
 echo "The addition of two numbers ${num1} and ${num2} is $addition"
