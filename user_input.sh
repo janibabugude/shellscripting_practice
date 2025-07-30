20b6d5d638c781a2adb2ccf48aee2b605a0bbc84
janibabugude <janibabugude@gmail.com>
2025-07-30 14:41:06 +0530

    user_input.sh

--- user_input.sh
+++ user_input.sh
@@ -9,3 +9,5 @@ read -p "enter your first number: " num1
 read -p "enter your second number: " num2
 addition=$(($num1+$num2))
 echo "The addition of two numbers ${num1} and ${num2} is $addition"
+multipication=$(($num1 * $num2))
+echo -n "the multipilication of two numbers $num1 and $num2 is: $multiplication"
