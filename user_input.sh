52e22ca57df1504b30f28cc6e9cb0016527d4a5d
janibabugude <janibabugude@gmail.com>
2025-07-30 14:21:51 +0530

    user_input.sh

--- user_input.sh
+++ user_input.sh
@@ -3,5 +3,5 @@ echo "enter the number : "
 read num1
 echo "enter the number: "
 read num2
-addition=$(($num1)+($num2))
+addition=$(($num1+$num2))
 echo "The addition of two numbers ${num1} and ${num2} is $addition"
