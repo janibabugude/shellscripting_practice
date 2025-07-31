3c3cb1f41ec8efab779721587579af953db08f47
janibabugude <janibabugude@gmail.com>
2025-07-31 20:38:44 +0530

    conditional.sh

--- conditional.sh
+++ conditional.sh
@@ -2,7 +2,7 @@
 # learn about conditional statement
 # if then elif else
 read -p "enter a number: " number
-if [ $number == $number]
+if [ $number == $number]:
 then
     echo "number is matched"
 fi
