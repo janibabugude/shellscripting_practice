b3b87749977b3c18d6907fb45290780d5a1f6fe0
janibabugude <janibabugude@gmail.com>
2025-07-31 20:56:06 +0530

    conditional.sh

--- conditional.sh
+++ conditional.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 # learn about conditional statement
 # if then elif else
+:<<"comment"
 read -p "enter a number: " number
 if [ $number == $number ]
 then
     echo "number is matched ${number}"
+fi
+comment
+# to take the age and check age < or > = using if
+read -p"enter you age: " age
+
+if [ $age /< 10 ]; then
+    echo "you are child"
+elif [ $age /> 10 and $age /< 20 ];then
+    echo"you are a teenager.."
+elif [ $age -gt 20 -a $age -lt 30 ]; then
+    echo"you are adult"
+else
+    echo "you are older.."
 fi
