38a80242c259fcbb06f68da1499ee595bc9d8d8e
janibabugude <janibabugude@gmail.com>
2025-07-31 20:17:40 +0530

    update argument.sh

--- argumentvariable.sh
+++ argumentvariable.sh
@@ -6,7 +6,7 @@ variable_name2=$2
 echo "$1 and $variable_name2"
 echo "This is your script_file: $0"
 echo "number of argument passed: $#"
-#echo " this is last argument: ${!@}"
+echo " this is last argument: $!"
 #echo "what are the aruguments passed in between ${5-9}"
 echo "what arguments are passed by user is: $@"
 echo "what are the arguments are passed by user is: $*"
