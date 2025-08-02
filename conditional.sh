f2c543a760147a98f465ed9706864f178a499f20
janibabugude <janibabugude@gmail.com>
2025-08-02 17:08:41 +0530

    conditional.sh

--- conditional.sh
+++ conditional.sh
@@ -11,11 +11,11 @@ comment
 # to take the age and check age < or > = using if
 read -p"enter you age: " age
 
-if [ $age < 10 ];then
+if [ $age -le 10 ];then
     echo "you are child"
-elif [ $age > 10 -a $age < 20 ];then
+elif [ $age -gt 10 -a $age -lt 20 ];then
     echo "you are a teenager.."
-elif [ $age -gt 20 -a $age -lt 30 ];then
+elif [ $age -ge 20 -a $age -lt 30 ];then
     echo "you are adult"
 else
     echo "you are older.."
